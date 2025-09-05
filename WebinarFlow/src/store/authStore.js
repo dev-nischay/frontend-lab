1a2c3b1a5db0f267b2ce74df850f7b43dbd6cab5
dev-nischay <nischay.mehn@gmail.com>
2025-09-05 16:27:31 +0530

    fixed auth store

--- WebinarFlow/src/store/authStore.js
+++ WebinarFlow/src/store/authStore.js
@@ -1,11 +1,11 @@
 import { create } from "zustand";
-import { useNavigate } from "react-router-dom";
 
-let nav = useNavigate();
-const authStore = create((set) => ({
+export const authStore = create((set) => ({
   credentials: {},
-  saveDob: (dob) => set((state) => ({ credentials: { ...state, dob } })),
-  saveEmail: (email) => set((state) => ({ credentials: { ...state, email } })),
+  saveDob: (dob) =>
+    set((state) => ({ credentials: { ...state.credentials, ...dob } })),
+  saveEmail: (email) =>
+    set((state) => ({ credentials: { ...state.credentials, ...email } })),
 }));
 
 export const { credentials, saveDob, saveEmail } = authStore.getState();
