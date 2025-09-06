d337fd64257fd906b8b65aeb9bb33e5f21931edf
dev-nischay <nischay.mehn@gmail.com>
2025-09-06 21:49:50 +0530

    fixed container component

--- WebinarFlow/src/components/Container.jsx
+++ WebinarFlow/src/components/Container.jsx
@@ -1,7 +1,7 @@
 export default ({ children, className = "" }) => {
   return (
-    <div className="bg-[#012b5b] text-white flex justify-center items-center h-screen">
-      <div className={` w-96 h-1/2 ${className}`}>{children}</div>
+    <div className="bg-[#012b5b]  text-white flex justify-center items-center h-screen">
+      <div className={` w-96 h-1/2  ${className}`}>{children}</div>
     </div>
   );
 };
