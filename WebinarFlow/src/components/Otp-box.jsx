3f2f6dab4d0172e1c29a8db5148d351fc61046d0
dev-nischay <nischay.mehn@gmail.com>
2025-09-08 22:36:56 +0530

    fixed otp logic

--- WebinarFlow/src/components/Otp-box.jsx
+++ WebinarFlow/src/components/Otp-box.jsx
@@ -1,10 +1,26 @@
-export default ({ reference, isDone }) => {
+import { useState } from "react";
+
+export default ({ reference, isDone, prev }) => {
+  const [input, setInput] = useState("");
   return (
     <input
       type="text"
       ref={reference}
+      maxLength={1}
       onChange={(e) => {
-        isDone();
+        let value = e.target.value;
+        let regex = /^[1-9]$/;
+        if (regex.test(value)) {
+          setInput(e.target.value);
+          isDone();
+        }
+      }}
+      value={input}
+      onKeyDown={(e) => {
+        if (e.key === "Backspace") {
+          prev();
+          setInput("");
+        }
       }}
       className="w-8 h-10 border text-center border-gray-500 bg-[#18406b] m-3 rounded-lg"
     ></input>
