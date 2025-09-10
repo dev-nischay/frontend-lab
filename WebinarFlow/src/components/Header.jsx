0ce0b9af127147f6b967c00268c12a105583d724
dev-nischay <nischay.mehn@gmail.com>
2025-09-11 00:57:37 +0530

    added components

---
+++ WebinarFlow/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="h-[15%] w-full font-mono ">
+      <div className="flex justify-end p-2 ">Thursday,10 september</div>
+      <div className="text-center font-mono  text-2xl  ">Good morning user</div>
+    </div>
+  );
+};
---
+++ WebinarFlow/src/components/Schedule.jsx
@@ -0,0 +1,50 @@
+import calender from "/src/assets/calendar-days.svg";
+import down from "/src/assets/chevron-down.svg";
+import left from "/src/assets/move-left.svg";
+import right from "/src/assets/move-right.svg";
+import videoRed from "/src/assets/video-camera.png";
+import videoBlue from "/src/assets/video.png";
+
+export const Schedule = () => {
+  const buttons = [down, left, right];
+  return (
+    <div className="min-h-72 w-[90%] mx-auto border rounded-lg p-3 border-gray-400 bg-slate-200 ">
+      <div className="w-full flex p-2  gap-1 items-center bg-gray-300 mb-5 ">
+        <img src={calender} alt="calender-icon" width={25} />
+        <div>Monday, 14 October 2024</div>
+        {buttons.map((e) => (
+          <button>
+            <img src={e} />
+          </button>
+        ))}
+      </div>
+      <WebinarSlots state={"Live"} />
+      <WebinarSlots state={"Upcoming"} />
+      <WebinarSlots state={"Upcoming"} />
+    </div>
+  );
+};
+
+const WebinarSlots = ({ state }) => {
+  return (
+    <div className="flex border-black p-1  relative my-5">
+      <div className="flex flex-col gap-2 mr-3">
+        <div>11:30 AM</div>
+        <div className="text-xs text-gray-400">11:30 AM</div>
+      </div>
+      <div className="bg-cyan-400  absolute left-20 h-full w-[1px]"></div>
+      <div className="flex flex-col gap-2 ml-3 relative">
+        <div className="text-xs flex gap-2 text-gray-400">
+          {state}
+          <img
+            src={state === "Live" ? videoRed : videoBlue}
+            alt=""
+            width={15}
+          />
+        </div>
+        <div className="text-lg ">UX Webinar</div>
+      </div>
+      <div className="absolute inset-x-0 bg-gray-400 -bottom-[0.85rem] h-[1px] "></div>
+    </div>
+  );
+};
