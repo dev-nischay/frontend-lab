92c7d123ce214c80aa2dd54dede96cc0a44dcc22
dev-nischay <nischay.mehn@gmail.com>
2025-09-10 21:18:27 +0530

    updated Dashboard-page

--- WebinarFlow/src/pages/Dashboard.jsx
+++ WebinarFlow/src/pages/Dashboard.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
-import Sidebar from "../components/Sidebar";
+
+import Button from "../components/Button";
+import credit from "/src/assets/credit-card.svg";
+import file from "/src/assets/file-user.svg";
+import house from "/src/assets/house.svg";
+import settings from "/src/assets/settings.svg";
+import users from "/src/assets/users.svg";
 
 export default () => {
+  let icons = [credit, file, house, settings, users];
   return (
     <>
-      <div className={`h-screen bg-white dark:bg-neutral-700 `}>
-        <button
-          onClick={() => setDark((prev) => !prev)}
-          className="py-2 px-4 bg-white text-black dark:bg-black dark:text-white"
-        >
-          switch
-        </button>
+      <div className="h-screen  bg-gradient-to-tr from-slate-800 via-blue-950 to-slate-500">
+        <div className="max-w-full h-auto fixed  bottom-0 inset-x-0 bg-slate-200 flex p-5 justify-between ">
+          {icons.map((e) => (
+            <button className=" transition-all gap-10 p-2 rounded hover:bg-slate-400 ">
+              <img src={e} />
+            </button>
+          ))}
+        </div>
       </div>
     </>
   );
