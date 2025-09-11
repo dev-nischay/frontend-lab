9a795684507acb9cf9f4f75cc82db0e0298368fc
dev-nischay <nischay.mehn@gmail.com>
2025-09-12 01:14:57 +0530

    changed background

--- WebinarFlow/src/components/Container.jsx
+++ WebinarFlow/src/components/Container.jsx
@@ -1,6 +1,6 @@
 export default ({ children, className = "" }) => {
   return (
-    <div className="bg-[#012b5b]  text-white flex justify-center items-center h-screen">
+    <div className="bg-gradient-to-tr from-slate-800 via-blue-950 to-slate-500  text-white flex justify-center items-center h-screen">
       <div className={` w-96 h-1/2  ${className}`}>{children}</div>
     </div>
   );
