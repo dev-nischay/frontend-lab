bfe64754c556ae350c1568b1b1c4e044453ad2c7
dev-nischay <nischay.mehn@gmail.com>
2025-09-15 06:02:48 +0530

    updated config

--- leetCode-ui/tailwind.config.js
+++ leetCode-ui/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGrey: {
+          200: "rgb(26,26,26)",
+          300: "rgb(38,38,38)",
+          400: "rgb(40,40,40)",
+          450: "rgb(44,44,44)",
+          500: "rgb(51,51,51)",
+          550: "rgb(56,56,56)",
+          650: "rgb(68,67,67)",
+          700: "rgb(160,160,160)",
+        },
+      },
+    },
   },
   plugins: [],
 };
