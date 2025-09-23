519c8cd70e1ed477029dedbfcf264149d9d0bca7
dev-nischay <nischay.mehn@gmail.com>
2025-09-23 20:04:05 +0530

    added features

---
+++ leetCode-ui/src/pages/Side.jsx
@@ -0,0 +1,11 @@
+import { create } from "z";
+
+export default () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <div>hey this is nischay</div>
+      </div>
+    </div>
+  );
+};
